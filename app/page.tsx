e6c115e5ff05876427fefa8b00dda7bddcbb2643
Miguel Ortega <miguelow@Miguels-MacBook-Air.local>
2023-02-21 11:52:00 +0100

    configured next auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 
-import Link from "next/Link";
 
-export default function Home() {
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+  if(!res.ok){
+    console.log(res)
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const data: { id: number; title: string } [] = await getPosts()
+  console.log("data",data)
+
   return (
     <main className="py-4 px-48">
-      <Link className="bg-teal-400 text-black font-bold py-2 px-4 rounded-md" href={'/dashboard'}>Go to dashboard</Link>
+      {data.map(post => (
+        <h1>{post.title}</h1>
+      ))}
     </main>
   )
 }
